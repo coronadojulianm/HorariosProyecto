ec5d762e50fc1fa740a9396206357e2a235f5305
coronadojulianm <juliancoronadom@gmail.com>
2024-08-01 14:50:07 -0500

    registrar usuarios, cambios en la base de datos, añadir registro de contraseña con bcrypt, no deja registraar si el correo o contraseña estan ya registrados, si estan registrados pasa a la pagina login

--- src/app/api/personas/route.js
+++ src/app/api/personas/route.js
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import bcrypt from 'bcrypt'
 
 // Función para convertir BigInt a string en un objeto
 function serializeBigInt(obj) {
@@ -21,15 +22,61 @@ export async function GET() {
 export async function POST(request) {
   try {
     const data = await request.json();
-    const persona = await prisma.personas.create({
-      data: data
+    
+    // Búsqueda de usuario si ya está registrada la identificación
+    const iduserFound = await prisma.personas.findUnique({
+      where: {
+        identificacion: data.identificacion,
+      }
     });
+    if (iduserFound) {
+      return NextResponse.json(
+        { message: "Identificación de usuario ya registrada" }, 
+        { status: 400 }
+      );
+    }
+    
+    // Búsqueda si el usuario ya está registrado con el correo
+    const emailuserFound = await prisma.personas.findFirst({
+      where: {
+        correo: data.correo,
+      }
+    });
+    if (emailuserFound) {
+      return NextResponse.json(
+        { message: "Correo de usuario ya registrado" }, 
+        { status: 400 }
+      );
+    }
+    
+    // Encriptación de la contraseña
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const persona = await prisma.personas.create({
+      data: {
+        identificacion: data.identificacion,
+        nombres: data.nombres,
+        correo: data.correo,
+        telefono: data.telefono,
+        password: hashedPassword,
+        rol: data.rol,
+        cargo: data.cargo,
+        municipio: data.municipio,
+      }
+    });
+
     const serializedPersona = serializeBigInt(persona);
     return new NextResponse(JSON.stringify(serializedPersona), {
       headers: { "Content-Type": "application/json" },
-      status: 201
+      status: 201,
     });
+    
   } catch (error) {
-    return new NextResponse(error.message, { status: 500 });
+    console.error('Error creating persona:', error);
+    return new NextResponse(JSON.stringify({ error: error.message }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
   }
 }
+
