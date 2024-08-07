d0d860ccb08562787c9b4cab625e131fb4b78dbe
coronadojulianm <juliancoronadom@gmail.com>
2024-08-06 19:33:29 -0500

    format horarios vinculacion table

--- src/app/components/templates/vinculacion/page.jsx
+++ src/app/components/templates/vinculacion/page.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Sidebar from '../../organisms/Sidebar'; // Ajusta la ruta según la estructura de carpetas
-import VinculacionTable from '../../pages/vinculaciontable/vinculacionTable'; // Ajusta la ruta según la estructura de carpetas
+import Sidebar from "../../organisms/Sidebar"; // Ajusta la ruta según la estructura de carpetas
+import VinculacionTable from "../../pages/vinculaciontable/vinculacionTable"; // Ajusta la ruta según la estructura de carpetas
 
 export default function VinculacionTemplate() {
   return (
-    <div className='flex'>
+    <div className="flex">
       <Sidebar />
-      <main className='flex-1 h-[calc(100vh-7rem)] flex flex-col p-4'>
+      <main className="flex-1 h-[calc(100vh-7rem)] flex flex-col p-4">
         <div>
-          <h1 className='text-black text-5xl mb-4'>Vinculaciones</h1>
+          <h1 className="text-black text-5xl mb-4">Vinculaciones</h1>
           <VinculacionTable />
         </div>
       </main>
